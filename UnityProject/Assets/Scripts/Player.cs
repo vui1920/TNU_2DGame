139302a58c0a402135693eea61071954fcdd4f0b
vui1920 <56706659+vui1920@users.noreply.github.com>
2019-11-08 12:19:42 +0800

    123

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("速度")]
+    public float speed = 3.5f;  //浮點
+    [Header("跳躍")]
+    public int jump = 200;      //整型
+    [Header("是否在地板")]
+    public bool isGroud = false;//布爾
+    [Header("角色名稱")]
+    public string _name = "KID";//字符串
+}
